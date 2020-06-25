58aaa240e543e04b3baec604a9465ef044294061
Antonio <37681087+antoniorodriguez22@users.noreply.github.com>
2020-06-24 21:23:24 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  consumer_key:         'JjDuWJQNn1kCwPMS8IG1REgSP',
-  consumer_secret:      'Ep9FIW65MYRid3mjK6eM04K5SZ3sLpteumx419FSslT0gcQhcc',
-  access_token:         '1239703559098204161-ppAWT5XAAAfcKBfSQBcvsjh2Addeon',
-  access_token_secret:  'JcM5oJO5bs0VwxY7XjqoxnGxQMwGO3PAuPPYFmnJXQUIR',
+  consumer_key:         '',
+  consumer_secret:      '',
+  access_token:         '',
+  access_token_secret:  '',
 }
